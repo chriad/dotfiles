8aca615cd3b65ae0ef85f2339a68ee47bf928a32
chriad <chriad.mueller@stud.unibas.ch>
2021-09-08 01:02:04 +0200

    Custom script for fzf actions

---
+++ fzf-params.sh
@@ -0,0 +1,7 @@
+export FZF_DEFAULT_OPTS="--bind='f1:execute(less -f {}),`
+`ctrl-y:execute(echo {} | xclip -selection clipboard)+abort,`
+`ctrl-z:execute-silent(zathura {})+abort,`
+`ctrl-h:execute(python ~/fzf-optparse.py|less -f),`
+`ctrl-b:execute(echo {} | xargs -d '\n' feh -w),`
+`ctrl-b:execute(feh {}),`
+`ctrl-o:execute(cat {} >> ~/bla.file)+abort'"
