a312f81156d00b95d8807334d4abe16c0bb84e1c
chriad <chriad.mueller@stud.unibas.ch>
2021-09-09 20:19:00 +0200

    Browser settings

---
+++ user-overrides.js
@@ -0,0 +1,28 @@
+/*** MY OVERRIDES ***/
+user_pref("_user.js.parrot", "overrides section syntax error");
+
+/* override recipe: enable session restore ***/
+user_pref("browser.startup.page", 3); // 0102
+  // user_pref("browser.privatebrowsing.autostart", false); // 0110 required if you had it set as true
+  // user_pref("places.history.enabled", true); // 0862 required if you had it set as false
+  // user_pref("browser.sessionstore.privacy_level", 0); // 1021 optional [to restore extras like cookies/formdata]
+user_pref("privacy.clearOnShutdown.history", false); // 2803
+  // user_pref("privacy.clearOnShutdown.cookies", false); // 2803 optional
+  // user_pref("privacy.clearOnShutdown.formdata", false); // 2803 optional
+user_pref("privacy.cpd.history", false); // 2804 to match when you use Ctrl-Shift-Del
+  // user_pref("privacy.cpd.cookies", false); // 2804 optional
+  // user_pref("privacy.cpd.formdata", false); // 2804 optional
+
+
+/*** [SECTION 0800]: LOCATION BAR / SEARCH BAR / SUGGESTIONS / HISTORY / FORMS ***/
+user_pref("_user.js.parrot", "0800 syntax error: the parrot's ceased to be!");
+user_pref("keyword.enabled", true); // 0801 - I use startpage
+
+
+// https://support.mozilla.org/en-US/questions/1263537
+user_pref("browser.tabs.insertRelatedAfterCurrent", true)
+user_pref("browser.tabs.insertAfterCurrent", true)
+
+user_pref("_user.js.parrot", "overrides section successful");
+
+
