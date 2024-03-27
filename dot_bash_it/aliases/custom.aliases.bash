1a0cbaf8ad10f059146b88fac1e6c6f1363435ab
chriad <chriad.mueller@protonmail.com>
2024-03-27 17:41:46 +0100

    Run emacs in daemon mode

--- dot_bash_it/aliases/custom.aliases.bash
+++ dot_bash_it/aliases/custom.aliases.bash
@@ -6,7 +6,7 @@ alias gedit=gnome-text-editor
 
 # 1 letter
 alias n='nautilus . &'
-alias e="emacsclient --no-wait -c"
+alias e="emacsclient --no-wait --create-frame --alternate-editor="
 alias a="alias|fzf"
 alias T="task|fzf"
 alias zz="ls|fzf"
