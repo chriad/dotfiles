16d77903f30c80da0da8f2872db58defc4afa3f0
chriad <chriad.mueller@protonmail.com>
2024-05-04 10:43:58 +0200

    combine ssh and wake on lan

--- dot_bashrc.d/remotes.bash
+++ dot_bashrc.d/remotes.bash
@@ -7,3 +7,9 @@ wolan() {
     local x=`cat /etc/hosts | egrep -v '^#' | cut -d" " -f3 | egrep -v '^$' | uniq | fzf`
     wakeonlan -f ~/.config/wol/"${x}".wol
 }
+
+con() {
+   local x=`cat /etc/hosts | egrep -v '^#' | cut -d" " -f3 | egrep -v '^$' | uniq | fzf`
+   ping -c1 "$x" >/dev/null 2>&1 || { wakeonlan -f ~/.config/wol/"${x}".wol; until ping -c1 "$x" >/dev/null 2>&1; do echo -n "."; done; echo ; }
+   kitten ssh "$x"
+}
