c147014c7b0a67f231146c041c21d6eaa3b811fe
chriad <chriad.mueller@protonmail.com>
2024-06-01 10:25:49 +0200

    news

--- private_dot_local/bin/executable_adb-push-file-quodlibet.sh
+++ private_dot_local/bin/executable_adb-push-file-quodlibet.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-/usr/lib/android-sdk/platform-tools/adb push --sync "${1}" /sdcard/Audiobooks
+# single song selected: select this file
+# playlist selected: sync all in playlist
+# select on album, push whole album, but without parent folder. To also sync folder, use adb-push-album-quodlibet.sh
+
+# TODO check if android daemon connected over tcp/ip
+output=$(/usr/lib/android-sdk/platform-tools/adb push --sync "${@}" /storage/emulated/0/Podcasts | tail -n 1)
+notify-send -t 2000 --app-name="quodlibet" "adb push" "${output}"
