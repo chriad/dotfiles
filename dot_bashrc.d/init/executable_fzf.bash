00bd2e34e346c6701d10abafd930bf3d75443c88
chriad <chriad.mueller@protonmail.com>
2024-08-06 07:18:56 +0200

    fzf tab completion

--- dot_bashrc.d/init/executable_fzf.bash
+++ dot_bashrc.d/init/executable_fzf.bash
@@ -1,3 +1,5 @@
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
 export FZF_DEFAULT_OPTS="--multi --bind='f1:execute(less -f {}),`
 `ctrl-y:execute(echo {} | xclip -selection clipboard)+abort,`
 `ctrl-z:execute-silent(zathura {})+abort,`
@@ -10,3 +12,6 @@ export FZF_DEFAULT_OPTS="--multi --bind='f1:execute(less -f {}),`
 `ctrl-o:execute(cat {} >> ~/bla.file)+abort'"
 
 # use ctrl+m with alias cm
+
+source $chriad_software/fzf-tab-completion/bash/fzf-bash-completion.sh
+bind -x '"\t": fzf_bash_completion'
