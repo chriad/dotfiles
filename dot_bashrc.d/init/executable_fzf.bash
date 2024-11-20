6360c139b8e243b4f4818752b13f61d4e29f7374
chriad <chriad.mueller@protonmail.com>
2024-11-20 10:57:56 +0100

    search pdf highlight text with fzf

--- dot_bashrc.d/init/executable_fzf.bash
+++ dot_bashrc.d/init/executable_fzf.bash
@@ -28,3 +28,22 @@ koreader--ebook-view ()
     d=/media/chriad/ssd-45/KOReader-Books
     fd --prune -e epub --base-directory $d | fzf --bind="enter:become(koreader /media/chriad/ssd-45/KOReader-Books/{} 2>&1 > /dev/null & disown)"
 }
+
+rga-fzf-nonbinary() {
+	  RG_PREFIX="rga --files-with-matches"
+	  local file
+	  file="$(
+		FZF_DEFAULT_COMMAND="$RG_PREFIX '$1'" \
+			fzf --sort --preview="[[ ! -z {} ]] && rga --pretty --context 5 {q} {}" \
+				--phony -q "$1" \
+				--bind "change:reload:$RG_PREFIX {q}" \
+				--preview-window="70%:wrap"
+	)" &&
+	      echo "opening $file" &&
+	      xdg-open "$file"
+}
+
+fzf--annots() {
+    pdf-highlight-annot-text-handler.py "${1}" |fzf -d"\t"
+    
+}
---
+++ private_dot_local/bin/executable_pdf-highlight-annot-text-handler.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import fitz
+import argparse
+# from PIL import Image
+
+parser = argparse.ArgumentParser(
+                    description='Extract highlighted text from a pdf.')
+
+parser.add_argument('infile')
+args = parser.parse_args()
+
+doc = fitz.open(args.infile)
+
+pages = doc.pages() # generator
+[print(str.replace(p.get_text(sort=True,clip=annot.rect), "\n", "　") + '\t') for p in doc.pages() for annot in p.annots()]
