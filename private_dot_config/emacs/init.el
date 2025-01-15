322d7a8ef16804d02d8628a1f395109f435badb1
chriad <chriad.mueller@protonmail.com>
2025-01-15 14:23:31 +0100

    activities.el

--- private_dot_config/emacs/init.el
+++ private_dot_config/emacs/init.el
@@ -156,6 +156,7 @@ This function should only modify configuration layer settings."
    ;; `:location' property: '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
    dotspacemacs-additional-packages '(
+                                      activities
                                       counsel-jq
                                       ;; TODO
                                       (org-anki :location (recipe
@@ -1187,6 +1188,24 @@ before packages are loaded."
   ;;       (progn
   ;;         (org-babel-next-src-block)
   ;;         (org-edit-special))))
+
+  (use-package activities
+    :init
+    (activities-mode)
+    (activities-tabs-mode)
+    ;; Prevent `edebug' default bindings from interfering.
+    (setq edebug-inhibit-emacs-lisp-mode-bindings t)
+
+    :bind
+    (("C-x C-a C-n" . activities-new)
+     ("C-x C-a C-d" . activities-define)
+     ("C-x C-a C-a" . activities-resume)
+     ("C-x C-a C-s" . activities-suspend)
+     ("C-x C-a C-k" . activities-kill)
+     ("C-x C-a RET" . activities-switch)
+     ("C-x C-a b" . activities-switch-buffer)
+     ("C-x C-a g" . activities-revert)
+     ("C-x C-a l" . activities-list)))
 
   (use-package org-fc
     :defer t
